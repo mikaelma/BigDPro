4e0e87e10a2f02c17033d90228ed8f03f80a42b2
Mikael Mathisen <mikaelma@stud.ntnu.no>
2018-03-08 14:58:10 +0100

    T2 DONE

--- task2.py
+++ task2.py
@@ -4,12 +4,15 @@ sc = SparkContext(appName="tweets")
 distFile = sc.textFile("data/geotweets.tsv")
 
 tweets = distFile.map(lambda line: line.split('\t'))
+country = tweets.map(lambda tweet: tweet[1])
+dist_countries = country.distinct().count()
+
+tweets = distFile.map(lambda line: line.split('\t'))
 country = tweets.map(lambda tweet: (tweet[1],1))
-sumTweets = country.reduceByKey(lambda a, b: a + b).sortBy(lambda a: a[1], ascending=False)
-print(sumTweets.collect())
+sumTweets = country.reduceByKey(lambda a, b: a + b)
+multipleSort = sumTweets.takeOrdered(dist_countries, key=lambda x: (-1 * x[1], x[0]))
 
-
-result = []
+result = [multipleSort]
 sc.parallelize(result).coalesce(1).saveAsTextFile('Results/Task2')
 
 sc.stop()
